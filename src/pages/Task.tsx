6626a335ab0920db32beae2afa6b99cf25f55176
ahabibi <a-habibi@sos.com>
2025-10-27 09:09:10 +0330

    feat: vite config

--- src/pages/Task.tsx
+++ src/pages/Task.tsx
@@ -2,7 +2,7 @@ import { Typography, Box } from "@mui/material";
 
 export default function Task() {
   return (
-    <Box sx={{ p: 3 }}>
+    <Box sx={{ p: 2 }}>
       <Typography variant="h4">Task</Typography>
     </Box>
   );
